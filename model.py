ad4e204eeff00eaea5fc189b51904ddd459423d9
Yvonne Yeh <yvonneyeh@me.com>
2021-02-03 20:56:15 +0000

    Add User, Movie, Rating models

--- model.py
+++ model.py
@@ -6,6 +6,53 @@ db = SQLAlchemy()
 
 
 # Replace this with your code!
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+
+class Movie(db.Model):
+    """A movie."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+
+
+class Rating(db.Model):
+    """A rating."""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
