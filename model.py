9207a52eed9f4ed4662336a6498427cfeec5059b
Yvonne Yeh <yvonneyeh@me.com>
2021-02-04 01:55:04 +0000

    Define functions in crud.py & seed_database.py

--- model.py
+++ model.py
@@ -17,6 +17,8 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    # ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
@@ -34,6 +36,8 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
 
+    # ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.title}>'
 
@@ -50,6 +54,8 @@ class Rating(db.Model):
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
 
     def __repr__(self):
         return f'<Rating rating_id={self.rating_id} score={self.score}>'
